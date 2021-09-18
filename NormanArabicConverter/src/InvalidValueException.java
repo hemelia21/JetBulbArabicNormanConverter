3a1410a2435d3a9f4723a545a0be1aa2a6ce6727
Дарья Словецких <dasha@Mac-mini-Dara.local>
2021-09-18 15:59:37 +0300

    corrected Exception

--- NormanArabicConverter/src/InvalidValueException.java
+++ NormanArabicConverter/src/InvalidValueException.java
@@ -1,4 +1,4 @@
-public class InvalidValueException extends Exception {
+public class InvalidValueException extends RuntimeException {
     public InvalidValueException(String message) {
         super(message);
     }
